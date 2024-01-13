9a4c33b160a7f4b586498da99f56a18243caeae3
Idowu Okusanya <jonlaney346@gmail.com>
2024-01-13 05:27:22 +0100

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 
 def search_replace(my_list, search, replace):
     new_list = [element for element in my_list]
-    
+
     for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
