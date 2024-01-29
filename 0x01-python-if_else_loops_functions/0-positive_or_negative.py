366b89ada2ea5e30f2f5f2cf65b33132ccc18ae0
idowujoshua1 <jonlaney346@gmail.com>
2024-01-29 06:07:32 +0000

    ‘0-positive_or_negative.py‘

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if x > 0:
+...     print('positive')
+... elif x == 0:
+...     print('Zero')
+... elif x < 0:
+...     print('Negative')
